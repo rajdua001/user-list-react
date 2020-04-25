f8aa2fad4dfa018de980e24038cc9f975beef679
Dua <Rajinder.dua@mail.nidec.com>
2020-04-24 21:46:38 -0400

    Updates

---
+++ precache-manifest.acffe47211d422e9b1f23e2d4611c5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e00183fa05c9f21f3761f217f65a75d",
+    "url": "/user-list-react/index.html"
+  },
+  {
+    "revision": "f46d828d04d0915b33dd",
+    "url": "/user-list-react/static/css/main.8b6c366f.chunk.css"
+  },
+  {
+    "revision": "4dc574f296a7f908aee4",
+    "url": "/user-list-react/static/js/2.0911daef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-list-react/static/js/2.0911daef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46d828d04d0915b33dd",
+    "url": "/user-list-react/static/js/main.42fc95c7.chunk.js"
+  },
+  {
+    "revision": "b4b9578d3fd8e2dd0d9f",
+    "url": "/user-list-react/static/js/runtime-main.631bcb36.js"
+  }
+]);
